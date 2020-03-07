eeef172ff3497571fac80c6c70abeabc5ee090bb
Lucas Stettner <lebra448@gmail.com>
2020-03-06 23:02:09 -0500

    Initial Commit

---
+++ CoronaTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoronaTracker
+//
+//  Created by Lucas Stettner on 3/6/20.
+//  Copyright © 2020 Lucas Stettner. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
